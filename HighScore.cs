552c7780ea774badd037219c6fe0cbee202e6a23
Rohans <rohanshastri00@gmail.com>
2013-03-25 09:22:49 -0700

    Create simple array, insertion method = ideal.

---
+++ HighScore.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace WumpHunt
+{
+    class HighScore
+    {   
+        int[]highScoreArray = new int[10];
+        
+        public int getHighScore(){
+            highScoreArray.OrderByDescending();
+
+        DB data = new DB(); 
+    }
+}
